eb0f8e4cacf3e5f83cbb931f5020431a95e7a5db
Jake-Holliman <88987855+Jake-Holliman@users.noreply.github.com>
2022-03-24 11:28:06 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+function Spa() {
+
+    
+
+    return (
+        <HashRouter>
+            <NavBar/>
+            <UserContext.Provider value={
+                {users:[
+                    { name:'Jake',
+                      email:'jgholliman@gmail.com',
+                      password:'password1',
+                      balance:999,
+                      history:[] }
+                ],
+                currentUser: null,
+                userIndex: null
+                }
+            }>
+            
+            <Route path="/" exact           component={Home}/>
+            <Route path="/createaccount/"   component={CreateAccount}/>
+            <Route path="/login/"           component={Login}/>
+            <Route path="/deposit/"         component={Deposit}/>
+            <Route path="/withdraw/"        component={Withdraw}/>
+            <Route path="/balance/"         component={Balance}/>
+            <Route path="/alldata/"         component={AllData}/>
+            </UserContext.Provider>
+        </HashRouter>
+    );
+}
+
+ReactDOM.render(
+    <Spa/>,
+    document.getElementById('root')
+);
