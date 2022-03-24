73f5b9e30bc3bc695cb386a6e20c8a667b2ca883
Jake-Holliman <88987855+Jake-Holliman@users.noreply.github.com>
2022-03-24 11:26:33 -0500

    Create home.js

---
+++ home.js
@@ -0,0 +1,17 @@
+
+// This document controls what is routed when Home is selected from the router
+function Home() {
+    return (
+       <Card
+            txtcolor="black"
+            header="Bad Bank Landing Page"
+            title="Welcome to the Bank"
+            text="You can use this bank"
+            body={<img src="/images/bank.png" style={{width: 250}}></img>}
+       
+       
+       
+       
+       />
+    );
+}
